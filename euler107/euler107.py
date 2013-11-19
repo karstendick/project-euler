201ed6908cd8c63c9cfb622a4ee8b488dc1729ef
karstendick <joshdick@gmail.com>
2013-11-19 12:09:34 -0500

    Completed PE #107

--- euler107/euler107.py
+++ euler107/euler107.py
@@ -1,54 +1,57 @@
-#Project Euler problem #107
+#PE #107
 
-INF = 1000
+#determines whether we run the small 7x7 case or the full 40x40 case
+DEBUG = False
 
-
-
-C = []
-#m = 0
-total_cost = 0
-for line in open('network_min.txt','r'):
-    c = []
-    for e in line.split(','):
-        if e == '-' or e == '-\r\n' or e == '-\n':
-            c.append(INF)
-        else:
-            c.append(int(e))
-            total_cost += int(e)
-    #m = max(m,max(c))
-    C.append(c)
-    
-total_cost /= 2
+from pygraph.algorithms.minmax import minimal_spanning_tree
+from pygraph.classes.graph import graph
+from pygraph.mixins.labeling import labeling
 
 
-min_cost = 0
-n = len(C)
-lowcost = [0 for x in range(n)]
-closest = [0 for x in range(n)]
+def total_weight(g, refg):
+    total = 0
+    for edge in g.edges():
+        total += refg.edge_weight(edge)
+    return total/2 #This seems to be double-counting edges. E.g. (1,3) and (3,1)
 
-for i in range(2,n+1):
-    lowcost[i-1] = C[1-1][i-1]
-    closest[i-1] = 1
-print 'lowcost:\t',lowcost
-print 'closest:\t',closest
+N = 7 if DEBUG else 40
+filename = 'network_min.txt' if DEBUG else 'network.txt'
 
-for i in range(2,n+1):
-    mmin = lowcost[2-1]
-    k = 2
-    for j in range(3,n+1):
-        if lowcost[j-1] < mmin:
-            mmin = lowcost[j-1]
-            k = j
-    print k,closest[k-1]
-    print 'cost:\t', C[k-1][closest[k-1]-1]
-    
-    min_cost += C[k-1][closest[k-1]-1]
-    lowcost[k-1] = INF
-    for j in range(2,n+1):
-        if C[k-1][j-1] < lowcost[j-1] and lowcost[j-1] < INF:
-            print 'INNER LOOP:\t',k,j
-            lowcost[j-1] = C[k-1][j-1]
-            closest[j-1] = k
-    print 'lowcost:\t',lowcost
-    print 'closest:\t',closest
-print total_cost - min_cost
+matrix = []
+
+for row,line in enumerate(file(filename,'r')):
+    matrix.append([0]*N)
+    line = line.strip()
+    for col,cost in enumerate(line.split(',')):
+        if cost != '-' and col >= row:
+            matrix[row][col] = int(cost)
+            if DEBUG:
+                print(row, col, cost)
+
+
+g = graph()
+g.add_nodes(range(N))
+
+for r in xrange(N):
+    for c in xrange(r,N):
+        if matrix[r][c] != 0:
+            g.add_edge((r,c), matrix[r][c])
+            if DEBUG:
+                print(r, c, matrix[r][c])
+        
+            
+
+st = minimal_spanning_tree(g)
+ming = graph()
+ming.add_spanning_tree(st)
+
+if DEBUG:
+    print(st)
+    print()
+    print(ming)
+    print()
+
+print(total_weight(g,g))
+print(total_weight(ming,g))
+print(total_weight(g,g) - total_weight(ming,g))
+
